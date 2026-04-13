5466c47750a345a5a0505f5bd341e4a2e6a5bd31
Aisling Proulx <aislingproulx@outlook.com>
2026-04-13 17:07:29 -0400

    add tests and collection extensions

--- Malfunction.Result/IResult.cs
+++ Malfunction.Result/IResult.cs
@@ -2,8 +2,15 @@
 {
     public interface IResult<TPass, TFail>
     {
-        public sealed record Pass(TPass Value) : IResult<TPass, TFail>;
-        public sealed record Fail(TFail Value) : IResult<TPass, TFail>;
+        public sealed record Pass(TPass Value) : IResult<TPass, TFail>
+        {
+            public static implicit operator TPass(Pass pass) => pass.Value;
+        }
+        public sealed record Fail(TFail Value) : IResult<TPass, TFail>
+        {
+            public static implicit operator TFail(Fail fail) => fail.Value;
+
+        }
     }
 }
 
--- Malfunction.Result/IResultExtentions.cs
+++ Malfunction.Result/IResultExtentions.cs
@@ -3,41 +3,32 @@ namespace Malfunction.Result
 {
     public static class IResultExtentions
     {
-        public static IResult<TResult, TFail> Map<TPass, TFail, TResult>(this IResult<TPass, TFail> result, Func<TPass, TResult> mapFunc) => result switch
+        public static IResult<TResult, TFail> Map<TPass, TFail, TResult>(this IResult<TPass, TFail> result, Func<TPass, TResult> func) => result switch
         {
-            IResult<TPass, TFail>.Pass pass => new IResult<TResult, TFail>.Pass(mapFunc(pass.Value)),
+            IResult<TPass, TFail>.Pass pass => new IResult<TResult, TFail>.Pass(func(pass.Value)),
             IResult<TPass, TFail>.Fail fail => new IResult<TResult, TFail>.Fail(fail.Value),
             _ => throw new Exception("Unexpected validation result")
         };
 
-        public static IResult<TResult, TFail> Bind<TPass, TFail, TResult>(this IResult<TPass, TFail> result, Func<TPass, IResult<TResult, TFail>> bindFunc) => result switch
+        public static IResult<TResult, TFail> Bind<TPass, TFail, TResult>(this IResult<TPass, TFail> result, Func<TPass, IResult<TResult, TFail>> func) => result switch
         {
-            IResult<TPass, TFail>.Pass pass => bindFunc(pass.Value),
+            IResult<TPass, TFail>.Pass pass => func(pass.Value),
             IResult<TPass, TFail>.Fail fail => new IResult<TResult, TFail>.Fail(fail.Value),
             _ => throw new Exception("Unexpected result")
         };
 
-        public static IResult<TPass, TResult> MapFail<TPass, TFail, TResult>(this IResult<TPass, TFail> result, Func<TFail, TResult> mapFunc) => result switch
+        public static IResult<TPass, TResult> MapFail<TPass, TFail, TResult>(this IResult<TPass, TFail> result, Func<TFail, TResult> func) => result switch
         {
             IResult<TPass, TFail>.Pass pass => new IResult<TPass, TResult>.Pass(pass.Value),
-            IResult<TPass, TFail>.Fail fail => new IResult<TPass, TResult>.Fail(mapFunc(fail.Value)),
+            IResult<TPass, TFail>.Fail fail => new IResult<TPass, TResult>.Fail(func(fail.Value)),
             _ => throw new Exception("Unexpected validation result")
         };
 
-        public static IResult<TPass, TResult> BindFail<TPass, TFail, TResult>(this IResult<TPass, TFail> result, Func<TFail, IResult<TPass, TResult>> bindFunc) => result switch
+        public static IResult<TPass, TResult> BindFail<TPass, TFail, TResult>(this IResult<TPass, TFail> result, Func<TFail, IResult<TPass, TResult>> func) => result switch
         {
             IResult<TPass, TFail>.Pass pass => new IResult<TPass, TResult>.Pass(pass.Value),
-            IResult<TPass, TFail>.Fail fail => bindFunc(fail.Value),
+            IResult<TPass, TFail>.Fail fail => func(fail.Value),
             _ => throw new Exception("Unexpected validation result")
         };
-
-        public static IResult<TPass, TAccumulate> AggregateFail<TPass, TFail, TAccumulate>(this IResult<TPass, List<TFail>> result, TAccumulate seed, Func<TAccumulate, TFail, TAccumulate> func) => AggregateFail(result, seed, func);
-        public static IResult<TPass, TAccumulate> AggregateFail<TPass, TFail, TAccumulate>(this IResult<TPass, TFail[]> result, TAccumulate seed, Func<TAccumulate, TFail, TAccumulate> func) => AggregateFail(result, seed, func);
-        public static IResult<TPass, TAccumulate> AggregateFail<TPass, TFail, TAccumulate>(this IResult<TPass, IEnumerable<TFail>> result, TAccumulate seed, Func<TAccumulate, TFail, TAccumulate> func) => MapFail(result, x => x.Aggregate(seed, func));
-        
-        public static IResult<TPass, TFail> AggregateFail<TPass, TFail>(this IResult<TPass, List<TFail>> result, Func<TFail, TFail, TFail> func) => AggregateFail(result, func);
-        public static IResult<TPass, TFail> AggregateFail<TPass, TFail>(this IResult<TPass, TFail[]> result, Func<TFail, TFail, TFail> func) => AggregateFail(result, func);
-        public static IResult<TPass, TFail> AggregateFail<TPass, TFail>(this IResult<TPass, IEnumerable<TFail>> result, Func<TFail, TFail, TFail> func) => MapFail(result, x => x.Aggregate(func));
-      
     }
 }
